df84d40efb7e8bebbb41448fb7efec6be8d48f25
Dian Xiao <diiaann@gmail.com>
2016-02-07 13:48:58 -0800

    required stories

---
+++ assignment1/PhotosViewController.swift
@@ -0,0 +1,38 @@
+//
+//  PhotosViewController.swift
+//  assignment1
+//
+//  Created by CloudLabs, Inc. on 2/7/16.
+//  Copyright © 2016 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.contentSize = CGSize(width: 320, height: 673)        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
